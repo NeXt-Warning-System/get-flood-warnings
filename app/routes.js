31ec307067e31600ff54b3efafff6eeb72bbfda1
christina gyles <christina.gyles@environment-agency.gov.uk>
2021-06-08 14:48:18 +0100

    external users only, versions 1 - 3

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,16 @@ const filters = require('./filters')(process.env)
 
 // Add your routes here - above the module.exports line
 
+
+// all routes for registration v2
+router.use('/v2', require('./routes_v2'))
+
+
+// Route index page
+router.get('/', function (req, res) {
+    res.render('index')
+  })
+
 router.all('*', (req, res, next) => {
     req.session.data.postcodeNotFound = false
     next()
