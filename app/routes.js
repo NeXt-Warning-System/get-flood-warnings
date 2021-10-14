0e49dfca954b324ea2e0ba92462acc56fde68d65
Christina Gyles <christinagyles@Christinas-MacBook-Pro.local>
2021-10-14 18:56:42 +0100

    updates to version 6 and create version 7, then add in up to date content for versions 6 and 7, which are A and B options for magic link

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,8 @@ const filters = require('./filters')(process.env)
 router.use('/v2', require('./routes_v2')),
 router.use('/v4', require('./routes_v4')),
 router.use('/v5', require('./routes_v5')),
-router.use('/v6', require('./routes_v6'))
+router.use('/v6', require('./routes_v6')),
+router.use('/v7', require('./routes_v7'))
 
 
 
